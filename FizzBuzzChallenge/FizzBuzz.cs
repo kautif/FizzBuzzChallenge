a2cd45a2186b14fafcf2219876b1963c4a7890ff
Autif Kamal <kautif@gmail.com>
2023-04-07 14:56:28 -0500

    return fizz buzz or both depending on being divisible by 3 5 or both

---
+++ FizzBuzzChallenge/FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzChallenge
+{
+    public class FizzBuzz
+    {
+        public string checkFizzBuzz(int num) {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+                return "fizzbuzz";
+
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+                return "buzz";
+            }
+            else if (num % 3 == 0) {
+                Console.WriteLine("fizz");
+                return "fizz";
+            }
+            else
+            {
+                Console.WriteLine("nothin   ");
+                return "";
+            }
+        }
+    }
+}
---
+++ FizzBuzzChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FizzBuzzChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.checkFizzBuzz(5);
+        }
+    }
+}
